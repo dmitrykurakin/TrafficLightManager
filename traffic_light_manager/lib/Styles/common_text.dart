028bc770f14e99c58f0f5b926196434b2fc5cd34
User <test@test.ru>
2024-09-23 19:34:38 +0300

    nullable check

---
+++ traffic_light_manager/lib/Styles/common_text.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CommonText extends StatelessWidget {
+  final String text;
+  final double? size;
+  final bool? isBold;
+
+  const CommonText({
+    super.key,
+    required this.text,
+    this.size,
+    this.isBold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    late double fontSize;
+    late FontWeight fontWeight;
+
+    size == null ? fontSize = 15 : fontSize = size!;
+
+    if (isBold == null) {
+      fontWeight = FontWeight.normal;
+    } else if (!isBold!) {
+      fontWeight = FontWeight.normal;
+    } else {
+      fontWeight = FontWeight.bold;
+    }
+
+    return Text(
+      text,
+      style: GoogleFonts.ubuntu(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
+}
